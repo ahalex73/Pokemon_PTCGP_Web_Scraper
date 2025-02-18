def7f76cac2783d1f9792a06b90ad5b4eaee161f
Alexander Holmes <78935210+ahalex73@users.noreply.github.com>
2025-02-17 23:11:44 -0500

    Refactored BASE_DIRS, Checkpoint before json

--- src/scraper.py
+++ src/scraper.py
@@ -6,10 +6,11 @@ from io import BytesIO
 import time
 
 BASE_URL = "https://pocket.limitlesstcg.com/cards"
-script_dir = os.path.dirname(os.path.abspath(__file__))  
-card_data = os.path.join(script_dir, "../pokemon_data/card_data")  
-images_base_dir = os.path.join(script_dir, "../pokemon_data/images")  
-os.makedirs(images_base_dir, exist_ok=True) 
+SCRIPT_BASE_DIR = os.path.dirname(os.path.abspath(__file__))  
+CARD_DATA_BASE_DIR = os.path.join(SCRIPT_BASE_DIR, "../pokemon_data/card_data")  
+IMAGES_BASE_DIR = os.path.join(SCRIPT_BASE_DIR, "../pokemon_data/images")  
+os.makedirs(IMAGES_BASE_DIR, exist_ok=True) 
+os.makedirs(CARD_DATA_BASE_DIR, exist_ok=True) 
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
@@ -45,7 +46,7 @@ def scrape_pokemon_data(soup, urls):
         soup = BeautifulSoup(response.text, "html.parser")
         target_div = soup.find("div", class_="card-search-grid")
         expansion_name = soup.find("div", class_= 'infobox-heading sm').text
-        image_dir = os.path.join(script_dir, "../pokemon_data/images/" + expansion_name)  
+        image_dir = os.path.join(IMAGES_BASE_DIR + "/" + expansion_name)  
         os.makedirs(image_dir, exist_ok=True) 
 
         if target_div:
@@ -67,8 +68,7 @@ def scrape_pokemon_data(soup, urls):
                 # We are now on a specific pokemon page such as "https://pocket.limitlesstcg.com/cards/A2/1" - Oddish from Space-Time Smackdown (A2)
                 if response.status_code == 200:
                     print(f"\033[42m \U0001F4AF \033[0m Scraping {individual_pokemon_url}. Status code: {response.status_code}")
-                    _gather_pokemon_information(soup, expansion_name, image_dir, postfix)
-
+                    _get_pokemon_information(soup, expansion_name, image_dir, postfix)
 
                     response.close()
                 else:
@@ -80,8 +80,13 @@ def scrape_pokemon_data(soup, urls):
 
     return 
 
-def _gather_pokemon_information(soup, expansion_name, image_dir, postfix):
-    print("Trying to gather pokemon data... ")
+def _get_pokemon_information(soup, expansion_name, image_dir, postfix):
+    get_picture(soup, expansion_name, image_dir, postfix)
+    #get_json_info()
+
+    return 
+
+def get_picture(soup, expansion_name, image_dir, postfix):
     img = soup.find(class_="card shadow resp-w")
     pokemon_name = soup.find(class_= "card-text-name").text
 
@@ -90,35 +95,72 @@ def _gather_pokemon_information(soup, expansion_name, image_dir, postfix):
 
         img_response = requests.get(img_url, headers=headers)
         if img_response.status_code == 200:
-            img_data = BytesIO(img_response.content)  # Store image in memory
+            img_data = BytesIO(img_response.content)
 
             try:
                 print(postfix)
                 postfix = postfix.replace("/", "_")
                 pokemon_name = pokemon_name.replace(" ", "-")
 
-                # Construct the full file path
                 webp_path = os.path.join(image_dir, expansion_name, f"{pokemon_name}_{postfix}.webp")
 
-                # Check if the file exists *before* creating the directory
                 if os.path.exists(webp_path):
                     print(f"Image {webp_path} already exists, skipping download.")
-                    return  # Exit early, no need to create directories or save
+                    return  
 
                 else:
-                    # Ensure the directory exists only when saving a new file
                     os.makedirs(os.path.dirname(webp_path), exist_ok=True)
 
                     print(f"Saving image as {webp_path}")
                     img_obj = Image.open(img_data)
                     img_obj.save(webp_path, "WEBP")
 
+
             except Exception as e:
                 print(f"Failed to process {img_url}: {e}")
         else:
             print(f"Failed to download {img_url}. Status Code: {img_response.status_code}")
 
-    return 
+    return
+
+
+def get_json_info(soup, expansion_name,):
+    img = soup.find(class_="card shadow resp-w")
+    pokemon_name = soup.find(class_= "card-text-name").text
+
+    if "src" in img.attrs:
+        img_url = img["src"]
+
+        img_response = requests.get(img_url, headers=headers)
+        if img_response.status_code == 200:
+            img_data = BytesIO(img_response.content)
+
+            try:
+                print(postfix)
+                postfix = postfix.replace("/", "_")
+                pokemon_name = pokemon_name.replace(" ", "-")
+
+                webp_path = os.path.join(CARD_DATA_BASE_DIR, expansion_name, f"{pokemon_name}_{postfix}.webp")
+
+                if os.path.exists(webp_path):
+                    print(f"Image {webp_path} already exists, skipping download.")
+                    return  
+
+                else:
+                    os.makedirs(os.path.dirname(webp_path), exist_ok=True)
+
+                    print(f"Saving image as {webp_path}")
+                    img_obj = Image.open(img_data)
+                    img_obj.save(webp_path, "WEBP")
+
+
+            except Exception as e:
+                print(f"Failed to process {img_url}: {e}")
+        else:
+            print(f"Failed to download {img_url}. Status Code: {img_response.status_code}")
+
+    return
+
 
 
 if __name__=="__main__":
